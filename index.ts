bc2cb7c9f13331326275003cbf18323bc97ad995
Alberto Dencowski <alberto.dencowski@arc-one.com>
2023-06-26 14:11:07 -0400

    Initial commit

---
+++ index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import {FlowServiceImpl} from "./src/services/flow-service.impl";
+import {StoreServiceImpl} from "./src/services/store-service.impl";
+import {HttpServiceImpl} from "./src/services/http-service.impl";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser());
+
+const store = new StoreServiceImpl();
+const httpService = new HttpServiceImpl();
+const flowService = new FlowServiceImpl(httpService, store);
+
+app.get('/processes/:id',
+    (req, res) => store.findById(req.params['id'])
+);
+
+app.post('/processes', (req, res) => flowService.executeFlow(req.body))
+
+app.listen(9922);
